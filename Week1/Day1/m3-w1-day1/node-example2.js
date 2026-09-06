40a5009c56b5495c44a201b44a60269cb5a233ec
Chris Aguilar <111602643+caguilar59@users.noreply.github.com>
2026-09-06 16:07:13 -0700

    New Repo

---
+++ Week1/Day1/m3-w1-day1/node-example2.js
@@ -0,0 +1,10 @@
+const { PI } = Math;  // private to this file
+
+const area = (r) => PI * r ** 2;
+
+const circumference = (r) => 2 * PI * r;
+
+module.exports = {
+    area,
+    circumference
+};
